0de210fd9779177fed65c3abfaf7348bc45aa1d5
Crowdbotics <team@crowdbotics.com>
2019-04-03 18:27:51 +0000

    initial commit

---
+++ android/app/src/main/java/com/loaner_1922/MainActivity.java
@@ -0,0 +1,15 @@
+package com.loaner_1922;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "loaner_1922";
+    }
+}
